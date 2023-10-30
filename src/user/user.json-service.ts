c0c4d2e9d89e3a2a5041c18cf33b270233724995
Chocolatiines <pimooshwoom@gmail.com>
2023-10-30 20:24:21 +0100

    Ajout méthodes add et getById

--- src/user/user.json-service.ts
+++ src/user/user.json-service.ts
@@ -1,12 +1,41 @@
 import {UserService} from "./user.service";
 import {User} from "./user";
+import * as fs from "fs";
 
 export class UserJsonService implements UserService {
+    private users: User[] = [];
+    private readonly filePath: string = "src/user/user.json";
+
+    constructor() {
+        this.loadUsersFromFile();
+    }
+
+    private saveUsersToFile() {
+        fs.writeFileSync(this.filePath, JSON.stringify(this.users, null, 2));
+    }
+
+    private loadUsersFromFile() {
+        try {
+            const data = fs.readFileSync(this.filePath, "utf-8");
+            this.users = JSON.parse(data);
+        } catch (error) {
+            console.log("Error while loading users from file", error);
+        }
+    }
     add(username: string): User {
-        throw new Error("Method not implemented.");
+        const newUser: User = {
+            id: this.users.length + 1,
+            username: username,
+        };
+
+        this.users.push(newUser);
+        this.saveUsersToFile();
+
+        return newUser;
     }
 
     getById(id: number): User | null {
-        throw new Error("Method not implemented.");
+        const user = this.users.find((u) => u.id === id);
+        return user || null;
     }
 }
